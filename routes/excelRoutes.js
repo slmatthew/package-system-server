76785aa9448dc008ed2a4bddfbe517f44ebd0c38
Matvey <36668268+slmatthew@users.noreply.github.com>
2024-12-18 13:29:59 +0300

    auth fix for /export

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -5,11 +5,11 @@ const roleMiddleware = require('../middlewares/roleMiddleware');
 
 const router = express.Router();
 
-router.get('/packages', exporter.packages);
-router.get('/facilities', exporter.facilities);
-router.get('/status_history', exporter.statusHistory);
-router.get('/package_statuses', exporter.packageStatuses);
-router.get('/package_types', exporter.packageTypes);
-router.get('/users', exporter.users);
+router.get('/packages', authMiddleware, roleMiddleware('sorter'), exporter.packages);
+router.get('/facilities', authMiddleware, roleMiddleware('sorter'), exporter.facilities);
+router.get('/status_history', authMiddleware, roleMiddleware('sorter'), exporter.statusHistory);
+router.get('/package_statuses', authMiddleware, exporter.packageStatuses);
+router.get('/package_types', authMiddleware, exporter.packageTypes);
+router.get('/users', authMiddleware, roleMiddleware('admin'), exporter.users);
 
 module.exports = router;
